56a7a9f0655c88af65bb74262e1ed55ab475f999
danielwang <aa@bb.com>
2025-04-03 15:13:58 +0800

    commit

--- test-dd-consumer/src/main/java/com/dd/consumer/processor/TestConsumer.java
+++ test-dd-consumer/src/main/java/com/dd/consumer/processor/TestConsumer.java
@@ -6,52 +6,59 @@ import com.dd.common.DDResponseTwo;
 import com.dd.common.GlobalConfig;
 import com.dd.common.entity.UserInfo;
 import com.dd.consumer.repo.UserInfoRepo;
-import org.springframework.amqp.support.AmqpHeaders;
+import io.micrometer.observation.Observation;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.binder.BinderHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-import static io.netty.handler.codec.http.HttpHeaders.setHeader;
 
 @Configuration
 public class TestConsumer {
     @Autowired
     private UserInfoRepo userInfoRepo;
 
+    Tracer tracer = GlobalOpenTelemetry.getTracer("TestConsumer", "0.0.1");
     @Bean
     public Consumer<Message<DDRequest>> testConsume() {
         return message -> {
-            try {
+            Span span = tracer.spanBuilder("testConsumeSpan").startSpan();
+
+            try (Scope scope = span.makeCurrent()) {
                 long begin = System.currentTimeMillis() / 1000;
                 System.out.println("Begin wait testConsume - " + GlobalConfig.testConsumeDelay + " : " + begin);
                 TimeUnit.MILLISECONDS.sleep(GlobalConfig.testConsumeDelay);
                 long end = System.currentTimeMillis() / 1000;
                 System.out.println("End wait testConsume : " + end + " - " + (end - begin));
-
+                DDRequest request = message.getPayload();
+                span.setAttribute("MsgTestConsume", request.getName());
+                UserInfo userInfo = new UserInfo();
+                BeanUtils.copyProperties(request, userInfo);
+                System.out.println("testConsume - Receive a request:" + request);
+                userInfoRepo.save(userInfo);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } finally {
+                span.end();
             }
-            DDRequest request = message.getPayload();
-            UserInfo userInfo = new UserInfo();
-            BeanUtils.copyProperties(request, userInfo);
-            System.out.println("testConsume - Receive a request:" + request);
-            userInfoRepo.save(userInfo);
         };
     }
 
     @Bean
     public Function<Message<DDRequest>, Message<?>> testFunction() {
         return message -> {
-            try {
+            try  {
+
                 long begin = System.currentTimeMillis() / 1000;
                 System.out.println("Begin wait testFunction - " + GlobalConfig.testFunctionDelay + " : " + begin);
                 TimeUnit.MILLISECONDS.sleep(GlobalConfig.testFunctionDelay);
