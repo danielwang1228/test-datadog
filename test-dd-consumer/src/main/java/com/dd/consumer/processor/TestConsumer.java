2d66fcce98eed26e38cf5a49b3026c2d7f509adb
danielwang <aa@bb.com>
2025-04-07 14:11:09 +0800

    commit code

--- test-dd-consumer/src/main/java/com/dd/consumer/processor/TestConsumer.java
+++ test-dd-consumer/src/main/java/com/dd/consumer/processor/TestConsumer.java
@@ -57,40 +57,45 @@ public class TestConsumer {
     @Bean
     public Function<Message<DDRequest>, Message<?>> testFunction() {
         return message -> {
-            try  {
+            Span span = tracer.spanBuilder("testFunctionSpan").startSpan();
 
+            try (Scope scope = span.makeCurrent()) {
                 long begin = System.currentTimeMillis() / 1000;
                 System.out.println("Begin wait testFunction - " + GlobalConfig.testFunctionDelay + " : " + begin);
                 TimeUnit.MILLISECONDS.sleep(GlobalConfig.testFunctionDelay);
                 long end = System.currentTimeMillis() / 1000;
                 System.out.println("End wait testFunction : " + end + " - " + (end - begin));
+
+                DDRequest request = message.getPayload();
+                System.out.println("testFunction - Receive a request:" + request);
+
+                UserInfo userInfo = new UserInfo();
+                BeanUtils.copyProperties(request, userInfo);
+                // access DB
+                userInfoRepo.findById(request.getId()).ifPresent(u -> userInfo.setAge(u.getAge()));
+                userInfoRepo.save(userInfo);
+                span.setAttribute("MsgTestFunction", userInfo.getName());
+
+                MessageBuilder<?> messageBuilder;
+                if (request.getFlag() % 2 == 1) {
+                    DDResponseOne responseOne = new DDResponseOne();
+                    BeanUtils.copyProperties(request, responseOne);
+                    messageBuilder = MessageBuilder.withPayload(responseOne);
+                } else {
+                    DDResponseTwo ddResponseTwo = new DDResponseTwo();
+                    BeanUtils.copyProperties(request, ddResponseTwo);
+                    ddResponseTwo.setNickname(request.getName());
+                    messageBuilder = MessageBuilder.withPayload(ddResponseTwo);
+                }
+                Message<?> retMsg = messageBuilder.setHeader("header1", "headerValue1")
+                        .setHeader("sendTime", System.currentTimeMillis())
+                        .build();
+                return retMsg;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } finally {
+                span.end();
             }
-            DDRequest request = message.getPayload();
-            System.out.println("testFunction - Receive a request:" + request);
-
-            UserInfo userInfo = new UserInfo();
-            BeanUtils.copyProperties(request, userInfo);
-            // access DB
-            userInfoRepo.findById(request.getId()).ifPresent(u -> userInfo.setAge(u.getAge()));
-            userInfoRepo.save(userInfo);
-
-            MessageBuilder<?> messageBuilder;
-            if (request.getFlag() % 2 == 1) {
-                DDResponseOne responseOne = new DDResponseOne();
-                BeanUtils.copyProperties(request, responseOne);
-                messageBuilder = MessageBuilder.withPayload(responseOne);
-            } else {
-                DDResponseTwo ddResponseTwo = new DDResponseTwo();
-                BeanUtils.copyProperties(request, ddResponseTwo);
-                ddResponseTwo.setNickname(request.getName());
-                messageBuilder = MessageBuilder.withPayload(ddResponseTwo);
-            }
-            Message<?> retMsg = messageBuilder.setHeader("header1", "headerValue1")
-                    .setHeader("sendTime", System.currentTimeMillis())
-                    .build();
-            return retMsg;
         };
     }
 }
