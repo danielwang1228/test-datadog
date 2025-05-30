2e3caec122b1722db0d82f676c857ea2b6949a63
danielwang <aa@bb.com>
2025-05-30 16:39:04 +0800

    jjjkjj

--- test-dd-web/src/main/java/com/dd/web/controller/TigerController.java
+++ test-dd-web/src/main/java/com/dd/web/controller/TigerController.java
@@ -1,6 +1,7 @@
 package com.dd.web.controller;
 
 import com.dd.common.DDRequest;
+import com.dd.web.service.OtherService;
 import com.dd.web.service.TigerService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("tiger")
 public class TigerController {
+    private OtherService otherService = new OtherService();
     @Resource
     private TigerService tigerService;
 
     @PostMapping("eat")
     public String eat(@RequestBody DDRequest request) {
+        otherService.otherEat();
         return tigerService.tigerEat(request);
     }
 
     @PostMapping("run")
     public String run(@RequestBody DDRequest request) {
+        otherService.otherRun();
         return tigerService.tigerRun(request);
     }
 }
---
+++ test-dd-web/src/main/java/com/dd/web/service/OtherService.java
@@ -0,0 +1,13 @@
+package com.dd.web.service;
+
+public class OtherService {
+    public String otherEat() {
+        System.out.println("otherEat");
+        return "OtherService eat";
+    }
+
+    public String otherRun() {
+        System.out.println("otherRun");
+        return "OtherService eat";
+    }
+}
