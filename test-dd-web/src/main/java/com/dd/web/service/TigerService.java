d2a1b22cb9b1f1b2e628548b38929a52ea42dc1f
danielwang <aa@bb.com>
2025-06-03 09:22:05 +0800

    xx

--- test-dd-web/src/main/java/com/dd/web/service/TigerService.java
+++ test-dd-web/src/main/java/com/dd/web/service/TigerService.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TigerService {
+    private OtherService otherService = new OtherService();
+
     public String tigerEat(DDRequest request) {
+        otherService.otherRun();
         return this.getClass().getName() + " eat";
     }
 
     public String tigerRun(DDRequest request) {
+        otherService.otherRun();
         return this.getClass().getName() + " run";
     }
 }
